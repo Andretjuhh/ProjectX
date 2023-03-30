8db797f4aee5e9afe9c65898a74075159a045f97
Reno Welleman <R.P.J.Welleman@student.tudelft.nl>
2023-03-30 11:29:23 +0200

    Verschillende klanttypes toegevoegd

---
+++ Bedrijf.java
@@ -0,0 +1,24 @@
+public class Bedrijf extends Klant {
+    private String bedrijfNaam;
+    static private int korting = 21;
+
+    public Bedrijf(String email, int telefoonnummer, String bedrijfNaam) {
+        super(email, telefoonnummer);
+        this.bedrijfNaam = bedrijfNaam;
+    }
+
+    // getter
+    public String getBedrijfNaam() {
+        return bedrijfNaam;
+    }
+    
+    // setter
+    public void setBedrijfNaam(String bedrijfNaam) {
+        this.bedrijfNaam = bedrijfNaam;
+    }
+
+    // Korting functie
+    static public double berekenKorting() {
+        return Klant.berekenKorting(korting);
+    }
+}
