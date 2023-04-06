2ff9ebcd4b962cc269c59d1c035c30f3d8a00c2e
RWelleman <94045727+RWelleman@users.noreply.github.com>
2023-04-06 15:20:07 +0200

    Klanttypes (#2)

---
+++ src/Overheid.java
@@ -0,0 +1,24 @@
+public class Overheid extends Klant {
+    private String instantie;
+    static private int korting = 12;
+
+    public Overheid(String email, int telefoonnummer, String instantie) {
+        super(email, telefoonnummer);
+        this.instantie = instantie;
+    }
+
+    // getter
+    public String getInstantie() {
+        return instantie;
+    }
+
+    // setter
+    public void setInstantie(String instantie) {
+        this.instantie = instantie;
+    }
+
+    // Korting functie
+    static public double berekenKorting() {
+        return Klant.berekenKorting(korting);
+    }
+}
---
+++ src/Particulier.java
@@ -0,0 +1,35 @@
+public class Particulier extends Klant {
+    private String voornaam;
+    private String achternaam;
+    static private int korting = 5;
+
+    // constructor
+    public Particulier(String email, int telefoonnummer, String voornaam, String achternaam) {
+        super(email, telefoonnummer);
+        this.voornaam = voornaam;
+        this.achternaam = achternaam;
+    }
+
+    // getters
+    public String getVoornaam() {
+        return voornaam;
+    }
+
+    public String getAchternaam() {
+        return achternaam;
+    }
+
+    // settters
+    public void setVoornaam(String voornaam) {
+        this.voornaam = voornaam;
+    }
+
+    public void setAchternaam(String achternaam) {
+        this.achternaam = achternaam;
+    }
+
+    // Korting functie
+    static public double berekenKorting() {
+        return Klant.berekenKorting(korting);
+    }
+}
