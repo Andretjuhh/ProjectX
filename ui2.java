bb5f6d430db58a7ea5e1bf776dd8cdf4e0ea5790
Andretjuhh <andre.silvareis12@gmail.com>
2023-04-14 16:37:24 +0200

    main

---
+++ ui2.java
@@ -0,0 +1,79 @@
+public class ui2 {
+    
+    private static String[] statussen = new String[]{"Aangevraagd","Compleet"};
+
+    private int offerteNummer;
+    private String datum;
+    private Klant klant;
+    private Scheepsbouwer scheepsbouwer;
+    private Boot boot;
+    private String status;
+
+    public Offerte(String datum, Klant klant, Scheepsbouwer scheepsbouwer, Boot boot) {
+        offerteNummer = ((int) (Math.random() * 100000));
+        this.datum = datum;
+        this.klant = klant;
+        this.scheepsbouwer = scheepsbouwer;
+        this.boot = boot;
+        status = statussen[0];
+    }
+
+    public int getOfferteNummer() {
+        return offerteNummer;
+    }
+    public String getDatum() {
+        return datum;
+    }
+    public Klant getKlant() {
+        return klant;
+    }
+    public Scheepsbouwer getScheepsbouwer() {
+        return scheepsbouwer;
+    }
+    public Boot getBoot() {
+        return boot;
+    }
+    public String getStatus() {
+        return status;
+    }
+
+    public void setOfferteNummer(int offerteNummer) {
+        this.offerteNummer = offerteNummer;
+    }
+    public void setDatum(String datum) {
+        this.datum = datum;
+    }
+    public void setKlant(Klant klant) {
+        this.klant = klant;
+    }
+    public void setScheepsbouwer(Scheepsbouwer scheepsbouwer) {
+        this.scheepsbouwer = scheepsbouwer;
+    }
+    public void setBoot(Boot boot) {
+        this.boot = boot;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String printHead() {
+        return "Offerte " + offerteNummer + " Datum: " + datum + " Status: " + status;
+    }
+
+    public String print() {
+        String result = printHead() + "\n";
+        result += "Status: " + status + "\n";
+        result += "Klant: " + klant.getEmail() + "\n";
+        result += "Scheepsbouwer: " + scheepsbouwer.getVoornaam() + " " + scheepsbouwer.getAchternaam() + "\n";
+        result += boot.print();
+
+        result += "Totaalprijs: " + totaalPrijs();
+
+        return result;
+    }
+
+    public double totaalPrijs() {
+        return klant.berekenKorting() * boot.berekenTotaalPrijs();
+    }
+}
+
